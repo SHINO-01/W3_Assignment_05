9e76279ca2b4a2b4aaefa4de6dd07076673cc7e2
SHINO-01 <sakifhussain33@gmail.com>
2024-11-22 18:40:07 +0600

    small update

--- user_service/routes.py
+++ user_service/routes.py
@@ -65,7 +65,7 @@ def register():
               type: string
               example: "User already exists"
       403:
-        description: Only Authenticated Admins can create Admin Accounts
+        description: PROTECTION VIOLATION: Only Admins can create Admin Accounts
         schema:
           type: object
           properties:
